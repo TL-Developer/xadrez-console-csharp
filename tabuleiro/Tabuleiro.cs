c801b61c13a1cfc98d2ae6b3540bd0dd05170445
Tiago Lima <tiago.jlima@yahoo.com.br>
2019-09-14 12:04:05 -0300

    Sobrecarga peca e exception personalizada

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -19,10 +19,38 @@ namespace tabuleiro
         return this.pecas[linha, coluna];
       }
 
+      public Peca ObterPeca(Posicao posicao)
+      {
+        return this.pecas[posicao.linha, posicao.coluna];
+      }
+
       public void ColocarPeca(Peca peca, Posicao posicao)
       {
         this.pecas[posicao.linha, posicao.coluna] = peca;
         peca.posicao = posicao;
+      }
+
+      public bool ExistePecao(Posicao posicao)
+      {
+        ValidaPosicao(posicao);
+        return ObterPeca(posicao) != null;
+      }
+
+      public bool PosicaoValida(Posicao posicao)
+      {
+        if (posicao.linha < 0 || posicao.linha >= this.linhas || posicao.coluna < 0 || posicao.coluna >= this.colunas) {
+          return false;
+        }
+
+        return true;
+      }
+
+      public void ValidaPosicao(Posicao posicao)
+      {
+        if(!PosicaoValida(posicao))
+        {
+          throw new TabuleiroException("Posição inválida!");
+        }
       }
     }
 }
