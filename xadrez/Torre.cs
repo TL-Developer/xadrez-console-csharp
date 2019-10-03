d151d8d49536b13c390a59305974a5c04b8d16b3
Tiago Lima <tiago.jlima@yahoo.com.br>
2019-10-03 08:11:24 -0300

    Adicionando algumas posições para Torre

--- xadrez/Torre.cs
+++ xadrez/Torre.cs
@@ -15,9 +15,59 @@ namespace xadrez
         return "T";
       }
 
+      private bool PodeMover(Posicao posicao)
+      {
+        Peca peca = tabuleiro.ObterPeca(posicao);
+        return peca == null || peca.cor != this.cor;
+      }
+
       public override bool[,] MovimentosPossiveis()
       {
         bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];
+
+        Posicao posicao = new Posicao(0, 0);
+
+        // acima
+        posicao.DefinirValores(posicao.linha - 1, posicao.coluna);
+        while (tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+        {
+          mat[posicao.linhas, posicao.coluna] = true;
+
+          if (tabuleiro.ObterPeca(posicao) != null && tabuleiro.ObterPeca(posicao).cor != this.cor) {
+            break;
+          }
+
+          posicao.linha = posicao.linha + 1;
+        }
+
+        // direita
+        posicao.DefinirValores(posicao.linha, posicao.coluna + 1);
+        while (tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+        {
+          mat[posicao.linhas, posicao.coluna] = true;
+
+          if (tabuleiro.ObterPeca(posicao) != null && tabuleiro.ObterPeca(posicao).cor != this.cor)
+          {
+            break;
+          }
+
+          posicao.coluna = posicao.coluna + 1;
+        }
+
+        // esquerda
+        posicao.DefinirValores(posicao.linha, posicao.coluna - 1);
+        while (tabuleiro.PosicaoValida(posicao) && PodeMover(posicao))
+        {
+          mat[posicao.linhas, posicao.coluna] = true;
+
+          if (tabuleiro.ObterPeca(posicao) != null && tabuleiro.ObterPeca(posicao).cor != this.cor)
+          {
+            break;
+          }
+
+          posicao.coluna = posicao.coluna - 1;
+        }
+
         return mat;
       }
     }
